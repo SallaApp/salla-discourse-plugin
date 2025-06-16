aaf141cdeb8b97c16dd09bfb919fe753280fd514
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-06-16 14:29:23 +0500

    added dynamic emoji in topic list serializer and fixed increment view count API

--- app/serializers/topic_list_item_serializer_extension.rb
+++ app/serializers/topic_list_item_serializer_extension.rb
@@ -10,7 +10,7 @@ TopicListItemSerializer.class_eval do
       id: category_id,
       name: object.category&.name,
       only_admin_can_post: object.category&.groups&.exists?(name: "admins"),
-      emoji: Discourse.base_url_no_prefix + object.category&.uploaded_logo&.url.to_s
+			emoji: "https://community.salla.com/forum/#{object.category.id}.png"
     }
   end
 
