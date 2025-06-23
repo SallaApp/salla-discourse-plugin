74e9470615fd2092f1e1bba71dd69e977017baf0
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-06-23 17:53:42 +0500

    Removed extra code

--- app/serializers/basic_category_serializer_extension.rb
+++ app/serializers/basic_category_serializer_extension.rb
@@ -2,7 +2,7 @@
 
 BasicCategorySerializer.class_eval do
   attributes :only_admin_can_post,
-            :emoji
+             :emoji
 
   def emoji
     "https://community.salla.com/forum/#{object.id}.png"
@@ -10,13 +10,5 @@ BasicCategorySerializer.class_eval do
 
   def only_admin_can_post
     object.groups.exists?(name: "admins")
-  end
-end
-
-CategoryDetailedSerializer.class_eval do
-  attributes :custom_frontend_fields
-
-  def custom_frontend_fields
-    object._custom_fields.map { |f| [f.name, f.value] }.to_h
   end
 end
