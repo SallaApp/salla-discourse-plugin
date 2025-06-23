67e591a38d7958a1f5d53700d27ea636b6c80ba2
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-06-23 15:39:01 +0500

    Updated Email Interceptor and Added Custom frontend Field

--- app/serializers/basic_category_serializer_extension.rb
+++ app/serializers/basic_category_serializer_extension.rb
@@ -12,3 +12,11 @@ BasicCategorySerializer.class_eval do
     object.groups.exists?(name: "admins")
   end
 end
+
+CategoryDetailedSerializer.class_eval do
+  attributes :custom_frontend_fields
+
+  def custom_frontend_fields
+    object._custom_fields.map { |f| [f.name, f.value] }.to_h
+  end
+end
