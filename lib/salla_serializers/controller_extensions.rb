dfb8491f343bb8de75c5c522e126dd0f85f4f222
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-06-24 19:18:40 +0500

    Remove Extra code

--- lib/salla_serializers/controller_extensions.rb
+++ lib/salla_serializers/controller_extensions.rb
@@ -4,56 +4,10 @@ module SallaSerializers
   module ControllerExtensions
     def self.included(base)
       base.after_action :remove_users_from_response
-      base.after_action :append_defaults_in_custom_fields
       base.before_action :set_sentry_context
     end
 
     private
-
-		def append_defaults_in_custom_fields
-			begin
-				json = JSON.parse(response.body, symbolize_names: true)
-			rescue JSON::ParserError => e
-				Rails.logger.error("Failed to parse JSON response body: #{e.message}")
-				return
-			end
-
-			if json.dig(:category_list, :categories)
-				json[:category_list][:categories] = process_categories(json[:category_list][:categories])
-			end
-
-			response.body = JSON.generate(json)
-		end
-
-		def process_categories(categories)
-			return categories unless categories.is_a?(Array)
-
-			categories.map do |category|
-				category = process_category(category)
-				if category[:subcategory_list].is_a?(Array)
-					category[:subcategory_list] = process_categories(category[:subcategory_list])
-				end
-				category
-			end
-		end
-
-		def process_category(category)
-			return category unless category.is_a?(Hash)
-
-			custom_fields = category[:custom_fields] || {}
-			return category unless custom_fields.is_a?(Hash)
-
-			default_values = {
-				post_view: 'list',
-				tabs_view: false,
-				user_can_create_post: false,
-				show_main_post: false
-			}
-
-			category[:custom_fields] = custom_fields.compact.reverse_merge(default_values)
-
-			category
-		end
 
     def remove_users_from_response
 			return if admin_referer?
